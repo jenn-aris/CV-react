b28e06b361c5674332036dda55cf2a63ee1d4a2b
Jennaris <jennaris@pop-os.localdomain>
2020-08-24 17:43:14 +0200

    Updates

---
+++ precache-manifest.ab66b64f39b5ca39bec39a60ac9407af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4b9c66d6587c056c0215732c05adb2",
+    "url": "/CV-react/index.html"
+  },
+  {
+    "revision": "b0905b8f18a8417230c5",
+    "url": "/CV-react/static/css/main.0bf8d237.chunk.css"
+  },
+  {
+    "revision": "da7d6a860c796c856d7c",
+    "url": "/CV-react/static/js/2.065c63e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-react/static/js/2.065c63e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0905b8f18a8417230c5",
+    "url": "/CV-react/static/js/main.62747419.chunk.js"
+  },
+  {
+    "revision": "2f06f288af520d8afc07",
+    "url": "/CV-react/static/js/runtime-main.f01f81f7.js"
+  },
+  {
+    "revision": "87a7aa14f5c78b4ddc4f3881acd2703f",
+    "url": "/CV-react/static/media/freecodecamp.87a7aa14.jpg"
+  },
+  {
+    "revision": "d3bab1d572becf299ca250ccda34af48",
+    "url": "/CV-react/static/media/mail.d3bab1d5.png"
+  },
+  {
+    "revision": "75ef7faf14f51f85d6b58bf8140fd27b",
+    "url": "/CV-react/static/media/opquast-pic.75ef7faf.svg"
+  },
+  {
+    "revision": "77361fd31231525be927086de2f03613",
+    "url": "/CV-react/static/media/phone.77361fd3.png"
+  },
+  {
+    "revision": "1f679ddf3bb5ee330bae6cc9d724954d",
+    "url": "/CV-react/static/media/portefolio.1f679ddf.png"
+  }
+]);
